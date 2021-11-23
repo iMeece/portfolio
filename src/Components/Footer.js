99c015d5652d05ac22635ceb8a7a17f7de914d8b
iMeece <MeeceCoding@gmail.com>
2021-11-23 17:07:13 +0200

    Added footer styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import { FaCopyright } from 'react-icons/fa';
 const Footer = () => {
     return (
         <div id='footer' className='flex'>
+            <div id='footer-background'></div>
             <h6>Copyright <FaCopyright size={10} /> 2021 - <a href='https://github.com/iMeece'> iMeece</a></h6>
         </div>
     );
